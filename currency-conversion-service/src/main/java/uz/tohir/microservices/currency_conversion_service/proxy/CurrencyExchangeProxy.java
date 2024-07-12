7ac87b1a526d4e4252ae806a836f5cd94de70bf9
Takhir <guvalakat1603@gmail.com>
2024-07-12 21:25:02 +0500

    // CHANGE-KUBERNETES

--- currency-conversion-service/src/main/java/uz/tohir/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/uz/tohir/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import uz.tohir.microservices.currency_conversion_service.entity.CurrencyConversion;
 
-@FeignClient(name = "currency-exchange-service",url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service",url = "localhost:8000")
 //@FeignClient(name = "currency-exchange-service")
+
+// CHANGE-KUBERNETES
+@FeignClient(name = "currency-exchange-service",url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")

04d2378633db21a1f52f444e0382efaeb5e43e80
Takhir <guvalakat1603@gmail.com>
2024-07-20 15:54:33 +0500

    variable is changed

--- currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/uz/tohir/microservices/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -48,7 +48,7 @@ public class CurrencyExchangeController {
 
         //CHANGE-KUBERNETES
         String host = environment.getProperty("HOSTNAME");
-        String version = "v11";
+        String version = "v12";
 
         currencyExchange.setEnvironment(port+" "+version+" "+host);
 
